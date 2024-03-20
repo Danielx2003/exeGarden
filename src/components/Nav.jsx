daf7a8e37e2ff2580060b1417285e29eb701ec6d
Danielx2003 <ahipsterwalrus4@gmail.com>
2024-03-20 21:00:54 +0000

    changed backend link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,7 @@ export default function Nav() {
     useEffect(() => {
         const getUsername = async () => {
             const response = await axios.get(
-                `http://${IP}:8000/authentication/user/`,
+                `http://br408.pythonanywhere.com/authentication/user/`,
                 {'withCredentials': true}
             )
             .then((res) => res.data)
